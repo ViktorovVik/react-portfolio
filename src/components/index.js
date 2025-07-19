94041b95967727a7c5e500f366d70919a8e1ce71
Viktor Shuntikov <schuntikov.viktor@yandex.ru>
2025-07-19 23:58:08 +0700

    home section destop and mobile versions imlementation

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 export {default as Header} from "./header/Header";
 export {default as Navigation} from "./navigation/Navigation";
 export {default as Footer} from "./footer/Footer";
-export {default as Home} from "./home/Home";
+export {default as Home} from "./Home/Home";
 export {default as About} from "./about/About";
 export {default as Skills} from "./skills/Skills";
 export {default as Contact} from "./contact/Contact";
